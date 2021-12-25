91a137abda21169f2833870b958f130b6f980855
sadraakhtarshenas <96003970+sadraakhtarshenas@users.noreply.github.com>
2021-12-25 04:59:42 -0800

    Add files via upload

---
+++ tamrin5.py
@@ -0,0 +1,9 @@
+a = input("How many people are you?")
+c = input("Enter your grades!")
+d = a + c 
+if a + c:
+    print(c)
+if c > a:
+    print("nomre shoma khob ast.")
+if c < a:
+    print("nomre shoma khob nist.")
